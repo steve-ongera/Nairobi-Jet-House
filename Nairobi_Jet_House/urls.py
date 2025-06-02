3647f08080cf8a241f8c61f048f2f6524a28e60c
steve <gadafisteve001@gmail.com>
2025-06-02 12:48:39 +0300

    made changes

--- Nairobi_Jet_House/urls.py
+++ Nairobi_Jet_House/urls.py
@@ -11,3 +11,9 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+from django.conf.urls import handler404, handler500
+
+handler404 = 'myapplication.views.custom_page_not_found'
+handler500 = 'myapplication.views.custom_server_error'
--- myapplication/views.py
+++ myapplication/views.py
@@ -3173,3 +3173,11 @@ def financial_dashboard(request):
     }
 
     return render(request, 'dashboard/financial_dashboard.html', context)
+
+
+
+def custom_page_not_found(request, exception):
+    return render(request, '404.html', status=404)
+
+def custom_server_error(request):
+    return render(request, '500.html', status=500)
